876449ca044221391e79dfcd6c717d49e6e0d4e4
DMawi17 <asegda17@gmail.com>
2021-06-28 15:22:00 +0200

    async/await

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,43 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
 let promiseOfModal = new Promise(function (resolve) {
     window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
+        resolve(modal);
+    }, 1000 * 3);
 });
 
-promiseOfModal.then(function(val) {
+async function toAsync() {
+    const val = await promiseOfModal;
     console.log("User has been on the page for 60 seconds");
     val.style.display = "block";
-})
+}
+
+toAsync();
+
+const animation = document.getElementById("continue");
+
+function atAnimCompletion() {
+    return new Promise((resolve) => {
+        animation.addEventListener("animationend", () => {
+            animation.classList.add("btn-info");
+            resolve("Continue to subscribe");
+        });
+    });
+}
+
+async function an() {
+    await atAnimCompletion();
+    alert("Continue to subscribe");
+}
+an();
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
+    switch (e.target.className) {
         case "close":
         case "modal":
-        modal.style.display = "none"
-        break;
+            modal.style.display = "none";
+            break;
     }
-})
-
+});
